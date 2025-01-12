defafa2c00e50eaedbe11b25be7dd8818a053d8c
Cadel Saszik <sazc0805@gmail.com>
2025-01-12 11:30:04 -0500

    Account Framing Fix (Temporary)

--- Pickit/Screens/Account/AccountViewInformation.swift
+++ Pickit/Screens/Account/AccountViewInformation.swift
@@ -20,64 +20,57 @@ struct AccountViewInformation: View {
     
     var body: some View {
         ZStack {
-//            BackgroundView()
-            Color(.mainBackground)
+            BackgroundView()
             
             VStack {
-//                HeaderView(screenName: self.screenName,
-//                           date: self.date,
-//                           accountName: self.accountName,
-//                           isSubscribed: self.isSubscribed)
+                //                Spacer()
                 
-                VStack {
-    //                Spacer()
-                    
-                    Image(.accountIcon)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 160, height: 160)
-                        .background(.midBlue)
-                        .clipShape(.circle)
-                    
-                    Text(accountName)
-                        .font(Font.custom("Lexend", size: 24))
-                        .fontWeight(.bold)
-                        .foregroundStyle(LinearGradient(colors: [.billingBGLight, .billingBGDark], startPoint: .topLeading, endPoint: .bottomTrailing))
-                        .padding(.top, 5)
-                    Text(isSubscribed ? "PREMIUM ACCOUNT" : "STANDARD ACCOUNT")
-                        .font(Font.custom("Lexend", size: 12))
-                        .fontWeight(.bold)
-                        .foregroundStyle(.lightWhite)
-                        .padding(.top, -18)
-                    
-                    VStack() {
-                        FormEntryView(entryTitle: "Username", entryValue: $username)
-                        FormPasswordView(entryTitle: "Password", entryValue: $password)
-                        FormEntryView(entryTitle: "Email", entryValue: $email)
-                            .padding(.bottom, 15)
-                        
-                        HStack {
-                            Text("Terms and Conditions")
-                                .font(Font.custom("Lexend", size: 12).bold())
-                                .foregroundStyle(.lightBlue)
-                                .frame(width: UIScreen.main.bounds.width / 2)
-                            
-                            Text("Privacy Policy")
-                                .font(Font.custom("Lexend", size: 12).bold())
-                                .foregroundStyle(.lightBlue)
-                                .frame(width: UIScreen.main.bounds.width / 2)
-                        }
-                    }
-                    .foregroundStyle(.lightWhite)
-    //                .background(.mainBackground)
-    //                .scrollContentBackground(.hidden)
+                Image(.accountIcon)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 160, height: 160)
+                    .background(.midBlue)
+                    .clipShape(.circle)
+                
+                Text(accountName)
+                    .font(Font.custom("Lexend", size: 24))
+                    .fontWeight(.bold)
+                    .foregroundStyle(LinearGradient(colors: [.billingBGLight, .billingBGDark], startPoint: .topLeading, endPoint: .bottomTrailing))
                     .padding(.top, 5)
-                    .padding(.bottom, 40)
-    //                .contentMargins(.bottom, -20)
+                Text(isSubscribed ? "PREMIUM ACCOUNT" : "STANDARD ACCOUNT")
+                    .font(Font.custom("Lexend", size: 12))
+                    .fontWeight(.bold)
+                    .foregroundStyle(.lightWhite)
+                    .padding(.top, -18)
+                
+                VStack() {
+                    FormEntryView(entryTitle: "Username", entryValue: $username)
+                    FormPasswordView(entryTitle: "Password", entryValue: $password)
+                    FormEntryView(entryTitle: "Email", entryValue: $email)
+                        .padding(.bottom, 15)
+                    
+                    HStack {
+                        Text("Terms and Conditions")
+                            .font(Font.custom("Lexend", size: 12).bold())
+                            .foregroundStyle(.lightBlue)
+                            .frame(width: UIScreen.main.bounds.width / 2)
+                        
+                        Text("Privacy Policy")
+                            .font(Font.custom("Lexend", size: 12).bold())
+                            .foregroundStyle(.lightBlue)
+                            .frame(width: UIScreen.main.bounds.width / 2)
+                    }
                 }
-                .padding(.top, 20)
+                .foregroundStyle(.lightWhite)
+                //                .background(.mainBackground)
+                //                .scrollContentBackground(.hidden)
+                .padding(.top, 5)
+                .padding(.bottom, 40)
+                //                .contentMargins(.bottom, -20)
             }
+            .padding(.top, 20)
         }
+        .padding(.top, 100)
     }
 }
 
@@ -85,8 +78,8 @@ struct AccountViewInformation: View {
     ZStack {
         AccountViewInformation(screenName: "Account", date: getCurrentDate(), accountName: "Cadel Saszik", isSubscribed: true)
         HeaderView2Section(screenName: "Account", date: getCurrentDate(), accountName: "Cadel Saszik", isSubscribed: true, leftSection: "Information", rightSection: "Billing", leftSectionActive: true)
-        VStack {
-            NavbarView(selectedTab: .constant(4))
-        }
+//        VStack {
+//            NavbarView(selectedTab: .constant(4))
+//        }
     }
 }
