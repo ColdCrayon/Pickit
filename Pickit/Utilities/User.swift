306e4df0a4e283f5835bf6d0fdcd66cb7e3f9a59
Cadel Saszik <sazc0805@gmail.com>
2025-01-19 21:34:42 -0600

    User Struct

---
+++ Pickit/Utilities/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  Pickit
+//
+//  Created by Cadel Saszik on 1/19/25.
+//
+
+import Foundation
+
+struct User: Codable {
+    let id: String
+    let name: String
+    let email: String
+    let joined: TimeInterval
+}
