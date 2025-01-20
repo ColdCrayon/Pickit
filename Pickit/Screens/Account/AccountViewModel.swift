cb744dd7994d0dfb171b678605b874f2d9f121cf
Cadel Saszik <sazc0805@gmail.com>
2025-01-19 21:51:01 -0600

    Account login and registration functions

--- Pickit/Screens/Account/AccountViewModel.swift
+++ Pickit/Screens/Account/AccountViewModel.swift
@@ -5,6 +5,8 @@
 //  Created by Cadel Saszik on 1/17/25.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import SwiftUI
 
 final class AccountViewModel: ObservableObject {
@@ -13,19 +15,83 @@ final class AccountViewModel: ObservableObject {
     @Published var password: String = ""
     @Published var email: String = ""
     
-    func getUsername() -> String {
-        return self.username
+    @Published var errorMessageLogin: String = ""
+    @Published var errorMessageRegister: String = ""
+    
+    init() {}
+    
+    func register() {
+        guard validateRegister() else {
+            errorMessageRegister = "An error occured during registration"
+            return
+        }
+        
+        Auth.auth().createUser(withEmail: email, password: password) { [weak self] result, error in
+            guard let userId = result?.user.uid else {
+                return
+            }
+            
+            self?.insertUserRecord(id: userId)
+        }
     }
     
-    func getFullName() -> String {
-        return self.fullName
+    func insertUserRecord(id: String) {
+        let newUser = User(id: id,
+                           name: fullName,
+                           email: email,
+                           joined: Date().timeIntervalSince1970)
+        
+        let db = Firestore.firestore()
+        
+        db.collection("users")
+            .document(id)
+            .setData(newUser.asDictionary())
     }
     
-    func getPassword() -> String {
-        return self.password
+    func login() {
+        guard validateLogin() else {
+            errorMessageLogin = "An error occured during login"
+            return
+        }
+        
+        Auth.auth().signIn(withEmail: email, password: password)
     }
     
-    func getEmail() -> String {
-        return self.email
+    func validateLogin() -> Bool {
+        guard !email.trimmingCharacters(in: .whitespaces).isEmpty,
+              !password.trimmingCharacters(in: .whitespaces).isEmpty else {
+            errorMessageLogin = "Please fill in all fields"
+            return false
+        }
+        
+        // email@foo.com
+        guard email.contains("@"), email.contains(".") else {
+            errorMessageLogin = "Please enter a valid email"
+            return false
+        }
+        
+        return true
+    }
+    
+    func validateRegister() -> Bool {
+        guard !email.trimmingCharacters(in: .whitespaces).isEmpty,
+              !username.trimmingCharacters(in: .whitespaces).isEmpty,
+              !password.trimmingCharacters(in: .whitespaces).isEmpty,
+              !fullName.trimmingCharacters(in: .whitespaces).isEmpty else {
+            errorMessageRegister = "Please fill in all fields"
+            return false
+        }
+        
+        guard email.contains("@"), email.contains(".") else {
+            errorMessageRegister = "Please enter a valid email"
+            return false
+        }
+        
+        guard password.count >= 8 else {
+            errorMessageRegister = "Your password must be at least 8 characters"
+            return false
+        }
+        
+        return true
     }
 }
