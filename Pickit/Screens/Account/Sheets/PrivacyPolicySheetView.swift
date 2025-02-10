887a6284fed7e6c8e4dd7a66516835a9bfb72c0a
Cadel Saszik <sazc0805@gmail.com>
2025-02-09 23:36:51 -0600

    Privacy Policy Sheet View

--- Pickit/Screens/Account/Sheets/PrivacyPolicySheetView.swift
+++ Pickit/Screens/Account/Sheets/PrivacyPolicySheetView.swift
@@ -8,8 +8,43 @@
 import SwiftUI
 
 struct PrivacyPolicySheetView: View {
+    
+    @StateObject var model = ModelPP()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            BackgroundViewBilling()
+            
+            Rectangle()
+                .frame(width: screenWidth - 50, height: screenHeight * 0.7)
+                .cornerRadius(30)
+                .overlay {
+                    ScrollView {
+                        VStack {
+                            Text("Privacy Policy")
+                                .font(Font.custom("Lexend", size: 32))
+                                .fontWeight(.bold)
+                                .padding(.top, 32)
+                                .padding(.bottom, 8)
+                                .foregroundStyle(.black)
+                            
+                            Text(model.data)
+                                .frame(maxWidth: screenWidth)
+                                .font(Font.custom("Lexend", size: 14))
+                                .fontWeight(.bold)
+                                .foregroundStyle(.black)
+                        }
+                    }
+                    .scrollIndicators(.hidden)
+                    .padding([.leading, .trailing], 20)
+                    .background(
+                        RoundedRectangle(cornerRadius: 30, style: .continuous)
+                            .fill(
+                                .shadow(.inner(color: Color.black.opacity(0.8), radius: 10, x: 0, y: 0))
+                            )
+                    )
+                }
+        }
     }
 }
 
