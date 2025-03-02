31c8ab61757707869ec8b879428360c269c816ca
Cadel Saszik <sazc0805@gmail.com>
2025-03-01 19:32:01 -0600

    Checkbox style

---
+++ Pickit/Utilities/Extensions/ToggleStyle.swift
@@ -0,0 +1,27 @@
+//
+//  ToggleStyle.swift
+//  Pickit
+//
+//  Created by Cadel Saszik on 3/1/25.
+//
+
+import SwiftUI
+
+struct iOSCheckboxToggleStyle: ToggleStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        // 1
+        Button(action: {
+
+            // 2
+            configuration.isOn.toggle()
+
+        }, label: {
+            HStack {
+                // 3
+                Image(systemName: configuration.isOn ? "checkmark.square" : "square")
+
+                configuration.label
+            }
+        })
+    }
+}
