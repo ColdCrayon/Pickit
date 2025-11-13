6ff7fb4a5e953179015b86aba683b9e38a98c3b5
Cadel Saszik <sazc0805@gmail.com>
2025-11-13 13:33:56 -0600

    watchlist and mytickets sidebar buttons

--- web/source/components/navigation/SidebarNav.tsx
+++ web/source/components/navigation/SidebarNav.tsx
@@ -8,6 +8,8 @@ import {
   Book,
   Shield,
   FileText,
+  Ticket,
+  Eye,
 } from "lucide-react";
 import { useUserPlan } from "../../hooks/useUserPlan";
 
@@ -45,6 +47,22 @@ const SidebarNav: React.FC<SidebarNavProps> = ({
                   <HomeIcon className="w-5 h-5" /> <span>Home</span>
                 </>
               )}
+            </Link>
+
+            <Link
+              to="/my-tickets"
+              className="flex items-center space-x-3 py-3 px-4 rounded-xl hover:bg-white/10 transition"
+              onClick={() => setIsSidebarOpen(false)}
+            >
+              <Ticket className="w-5 h-5" /> <span>My Tickets</span>
+            </Link>
+
+            <Link
+              to="/watchlist"
+              className="flex items-center space-x-3 py-3 px-4 rounded-xl hover:bg-white/10 transition"
+              onClick={() => setIsSidebarOpen(false)}
+            >
+              <Eye className="w-5 h-5" /> <span>Watchlist</span>
             </Link>
 
             <Link
